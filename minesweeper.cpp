e2d2ef50b23211a38d04a7a8c17c3b5fa5a185e6
Mafin64 <115665242+Mafin64@users.noreply.github.com>
2025-04-10 12:18:04 +0200

    Update minesweeper.cpp
    pridana funkce na konec hry

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -5,8 +5,8 @@
 using std::cout;
 using std::cin;
 
-
 void vypispole(char hracipole[5][5]);
+void konechry(char hracipole[5][5]);
 int main()
 {
     char* loc = setlocale(LC_CTYPE, "");
@@ -30,42 +30,14 @@ int main()
         printf("Zadejte Y souřadnici: ");
         cin >> vstupY;
 
-        if (hracipole[vstupX][vstupY] == 'X')
+        if (hracipole[vstupY][vstupX] == 'X')
         {
-            system("cls");
-            cout << "    0 1 2 3 4 Y\n";
-            cout << "   " << "-----------\n";
-
-            for (int i = 0; i < 5; i++)
-            {
-                printf("%d |", i);
-
-                for (int j = 0; j < 5; j++)
-                {
-                    if (hracipole[i][j] == 'X')
-                    {
-                        cout << " X";
-                    }
-
-                    else if (hracipole[i][j] == '#')
-                    {
-                        cout << " #";
-                    }
-
-                    else
-                    {
-                        cout << " " << hracipole[i][j];
-                    }
-                }
-                puts("");
-            }
-
-            cout << "X\n\n";
+            konechry(hracipole);
             printf("Vybouch jsi.");
             return 1;
         }
 
-        if (hracipole[vstupX][vstupY] == '#')
+        if (hracipole[vstupY][vstupX] == '#')
         {
             printf("Nevybouch jsi, vyber další pozici.\n\n");
         }
@@ -82,14 +54,14 @@ int main()
             }
         }
 
-        hracipole[vstupX][vstupY] = bombyvokoli + '0';
+        hracipole[vstupY][vstupX] = bombyvokoli + '0';
         bombyvokoli = 0;
     }
 }
 
 void vypispole(char hracipole[5][5]) {
-    cout << "    0 1 2 3 4 Y\n";
-    cout << "   " << "-----------\n";
+    cout << "    0 1 2 3 4 X\n";
+    cout << "  " << char(218) << "-----------\n";
 
     for (int i = 0; i < 5; i++)
     {
@@ -113,5 +85,38 @@ void vypispole(char hracipole[5][5]) {
         }
         puts("");
     }
-    cout << "X\n\n";
+    cout << "Y\n\n";
+}
+
+void konechry(char hracipole[5][5])
+{
+    system("cls");
+    cout << "    0 1 2 3 4 X\n";
+    cout << "  " << char(218) << "-----------\n";
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d |", i);
+
+        for (int j = 0; j < 5; j++)
+        {
+            if (hracipole[i][j] == 'X')
+            {
+                cout << " X";
+            }
+
+            else if (hracipole[i][j] == '#')
+            {
+                cout << " #";
+            }
+
+            else
+            {
+                cout << " " << hracipole[i][j];
+            }
+        }
+        puts("");
+    }
+
+    cout << "Y\n\n";
 }
